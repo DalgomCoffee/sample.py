a7f9c0c5a0f3522f628b2ba9f1104f5b9e1a13ae
DalgomCoffee <152689726+DalgomCoffee@users.noreply.github.com>
2024-05-19 20:25:22 +0800

    Update application.py

--- application.py
+++ application.py
@@ -4,15 +4,15 @@ from tensorflow.keras.models import load_model
 import numpy as np
 from PIL import Image, UnidentifiedImageError
 
-model = load_model('bestmodel.h5')
-class_names = ['Rain', 'Shine', 'Cloudy', 'Sunrise']
-
 st.markdown("""
 ### Instructions:
 1. Upload the weather image (jpg, png, jpeg).
 
 2.Prediction will display
 """)
+
+model = load_model('bestmodel.h5')
+class_names = ['Rain', 'Shine', 'Cloudy', 'Sunrise']
 
 def preprocess_image(image, target_size=(60, 40)):
     image = image.resize(target_size)
@@ -22,13 +22,11 @@ def preprocess_image(image, target_size=(60, 40)):
     image = image.flatten()  
     image = np.expand_dims(image, axis=0)  
     return image
-
 def predict(image):
     p_image = preprocess_image(image)
     return model.predict(p_image)
 
 st.title("FINAL EXAM: Weather Prediction")
-
 uploaded_file = st.file_uploader("Upload a weather image", type=["jpg", "png", "jpeg"])
 
 if uploaded_file is not None:
