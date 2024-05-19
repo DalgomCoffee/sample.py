84dc24d92d645d42780261e855c6c095ef0bad49
DalgomCoffee <152689726+DalgomCoffee@users.noreply.github.com>
2024-05-19 18:16:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,8 @@ def import_and_predict(image_data,model):
     size=(60,40)
     image=image_data.resize(size)
     img=np.asarray(image)
-    img_reshape=img[np.newaxis,...]
+    img_flatttened=img.flatten()
+    img_reshape=img_flattened[np.newaxis,...]
     prediction=model.predict(img_reshape)
     return prediction
   
