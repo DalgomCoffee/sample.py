0e69be9a8734f78b5b31899c75173f33e1862c23
DalgomCoffee <152689726+DalgomCoffee@users.noreply.github.com>
2024-05-19 17:03:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from PIL import Image,ImageOps
 
 import numpy as np
 def import_and_predict(image_data,model):
-    size=(60,50)
+    size=(100,100)
     image=ImageOps.fit(image_data,size)
     img=np.asarray(image)
     img_reshape=img[np.newaxis,...]
