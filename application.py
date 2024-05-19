ae524f7b20c76978bb5468309c78fb550d018d4e
DalgomCoffee <152689726+DalgomCoffee@users.noreply.github.com>
2024-05-19 21:30:36 +0800

    Update application.py

--- application.py
+++ application.py
@@ -41,39 +41,3 @@ st.markdown("""
 1. Upload a weather image (jpg, png, jpeg).
 2. The prediction will be displayed.
 """)
-
-import requests
-from tkinter import *
-
-
-root=Tk()
-root.geometry("500x500")
-root.config(bg="black")
-root.title("weather app")
-
-enter_city=Entry(root,font="lucinda 20 bold")
-enter_city.pack()
-
-def search():
-    my_url="http://api.openweathermap.org/data/2.5/weather"
-    api_key="e170aa40f3631125e4a20447689e9eb7"
-    parameters={
-        "q":enter_city.get(),
-        "appid":api_key
-
-    }
-
-    response=requests.get(url=my_url,params=parameters)
-    temp_data_kelvin=response.json()["main"]["temp"]
-    temp_data_celsius=temp_data_kelvin-273.15
-
-    text_temp.config(text=f" current temparature \n of {enter_city.get()} is :{int(temp_data_celsius)} degree C")
-
-
-b1=Button(root,text="seach location",font="lucinda 30 bold",fg="black",bg="black",command=search)
-b1.pack(padx=20,pady=20)
-
-text_temp = Label(text=".....",font="lucinda 25 bold")
-text_temp.pack(padx=20, pady=20)
-
-root.mainloop()
