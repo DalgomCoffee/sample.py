0886f47e1ffaf90985171ba5258d6af3137f0bba
DalgomCoffee <152689726+DalgomCoffee@users.noreply.github.com>
2024-05-19 18:57:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import tensorflow as tf
 import cv2
+import io
+import base64
 
 @st.cache(allow_output_mutation=True)
 def load_model():
@@ -12,7 +14,7 @@ st.write("""
 # Plant Leaf Detection System"""
 )
 
-file=st.file_uploader("Choose plant photo from computer",type=["jpg","png"])
+file=st.file_uploader("Choose plant photo from computer",type=["jpg","png","jpeg"])
 
 from PIL import Image,ImageOps
 import numpy as np
