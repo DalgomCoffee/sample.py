542efd7125bf2b30c9e961b382a94007ae5f5456
DalgomCoffee <152689726+DalgomCoffee@users.noreply.github.com>
2024-05-19 17:42:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ def import_and_predict(image_data,model):
     size=(60,40)
     image=ImageOps.fit(image_data,size,Image.LANCZOS)
     img=np.asarray(image)
+    img=img / 255.0
     img_reshape=img[np.newaxis,...]
     prediction=model.predict(img_reshape)
     return prediction
