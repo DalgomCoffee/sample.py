3bb7400171203c64c7806ced7239c1ae31918d6c
DalgomCoffee <152689726+DalgomCoffee@users.noreply.github.com>
2024-05-19 17:20:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,9 @@ from PIL import Image,ImageOps
 import numpy as np
 def import_and_predict(image_data,model):
     size=(60,40)
-    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
+    image=ImageOps.fit(image_data,size,Image.LANCZOS)
     img=np.asarray(image)
+    img = img / 255.0
     img_reshape=img[np.newaxis,...]
     prediction=model.predict(img_reshape)
     return prediction
